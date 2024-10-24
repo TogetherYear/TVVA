13ce2f81f0b7e90f5aac49e76cf86276176737bc
TSingleton <togetheryear@gmail.com>
2024-10-24 09:27:42 +0800

    Update:Limit

--- Src/Plugins/Renderer.ts
+++ Src/Plugins/Renderer.ts
@@ -1,24 +1,30 @@
 import { Manager } from '@/Libs/Manager';
 import { TEvent } from '@/Decorators/TEvent';
-import * as Tauri from '@tauri-apps/api';
+import * as T from '@tauri-apps/api';
 
 @TEvent.Create(['Message', 'Empty'])
 class Renderer extends Manager {
     public get App() {
         return {
-            Invoke: (cmd: string, args?: Tauri.core.InvokeArgs) => {
-                return Tauri.core.invoke(cmd, args);
+            Invoke: (cmd: string, args?: T.core.InvokeArgs) => {
+                return T.core.invoke(cmd, args);
             }
+        };
+    }
+
+    public get Widget() {
+        return {
+            Listen: T.window.Window.getCurrent().listen.bind(T.window.Window.getCurrent())
         };
     }
 
     public get Event() {
         return {
-            Listen: Tauri.event.listen,
-            Once: Tauri.event.once,
-            Emit: Tauri.event.emit,
+            Listen: T.event.listen,
+            Once: T.event.once,
+            Emit: T.event.emit,
             TauriEvent: {
-                ...Tauri.event.TauriEvent,
+                ...T.event.TauriEvent,
                 TAURI: 'tauri://tauri'
             }
         };
@@ -36,7 +42,7 @@ class Renderer extends Manager {
     }
 
     private ListenEvents() {
-        this.Event.Listen<Record<string, unknown>>(this.Event.TauriEvent.TAURI, async (e) => {
+        this.Widget.Listen<Record<string, unknown>>(this.Event.TauriEvent.TAURI, async (e) => {
             const r = e.payload;
             if (r.event === this.RendererEvent.Empty) {
                 this.Emit(this.RendererEvent.Empty, r);
